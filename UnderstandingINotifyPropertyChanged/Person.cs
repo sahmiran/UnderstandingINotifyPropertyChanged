27883460ae4ff3085dec6dde27d47b928aa04ec6
sahmiran <hasancantural19@gmail.com>
2021-12-09 10:56:54 +0300

    Add project files.

---
+++ UnderstandingINotifyPropertyChanged/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingINotifyPropertyChanged
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string firstname;
+        private string lastname;
+        private string fullname;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set 
+            {
+                if (firstname != value)
+                { 
+                    firstname = value;
+                    OnPropertyChanged(nameof(FirstName));
+                    OnPropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastname; }
+            set
+            {
+                if (lastname != value)
+                { 
+                    lastname = value;
+                    OnPropertyChanged(nameof(LastName));
+                    OnPropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+
+        public string FullName
+        {
+            get { return fullname = firstname+ " " + lastname; }
+            set
+            {
+                if (fullname != value)
+                { 
+                    fullname = value;
+                    OnPropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
